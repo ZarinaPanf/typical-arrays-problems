3ca79758e9f1a0cddb72db6812fabea43c8e021f
Zarina <zhiyenbayeva.z@gmail.com>
2020-03-18 23:18:50 +0600

    init: resolve a task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNumber = 0;
+  if (!array || array === []) {
+    return 0;
+  }
+  for (let i = 0; i<array.length; i++) {
+    if (array[i]<minNumber) {
+      minNumber = array[i];
+    }
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxNumber = 0;
+  if (!array || array === []) {
+    return 0;
+  }
+  for (let i = 0; i<array.length; i++) {
+    if (array[i]>maxNumber) {
+      maxNumber = array[i];
+    }
+  }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sumNumber = 0;
+  let avgNumber = 0;
+  if (!array || array === []) {
+    return 0;
+  }
+  for (let i = 0; i<array.length; i++) {
+    sumNumber += array[i];
+    avgNumber = sumNumber / array.length;
+  }
+  return avgNumber;
 }
